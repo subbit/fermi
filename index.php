73da494d736173ecae549fdfaf7e1f73204e9678
Christian Seppa <tech@tanvan.com>
2013-11-05 15:55:28 -0800

    remove dirty words in german

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ h1 { font-family:#330000;}
 </style>
 </head>
 <body>
-Wilkommen komradz, du bist ein schweisskopf.<br>
+Wilkommen komradz, du bist ein kwatschkopf.<br>
 twas brillig and the slithy toves 1729 rosebud<br>
 Koala 1943 dee bee and not just a river in egypt
 <?php
