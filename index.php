34e9810ef96bc87d5bd4daa317147a5300150971
Christian Seppa <tech@tanvan.com>
2013-11-05 12:06:44 -0800

    Redundancy test

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>+++wilkommen+++</title>
+<style>
+body {background-color:#6699cc !important; text-align:center;}
+h1 { font-family:#330000;}
+</style>
+</head>
+<body>
+Wilkommen komradz, du bist ein schweisskopf.<br>
+twas brillig and the slithy toves 1729 rosebud<br>
+Koala 1943 dee bee and not just a river in egypt
+<?php
+phpinfo();
+?>
+</body>
+</html>
